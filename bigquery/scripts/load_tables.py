4beae745e7a341e1c103eecabfe761445021c1ed
jeffonelson <jeff@jeffonelson.com>
2019-06-04 09:49:52 -0700

    Cleans up table creation script

--- bigquery/scripts/load_tables.py
+++ bigquery/scripts/load_tables.py
@@ -1,26 +1,23 @@
-# Initialize BigQuery environment
 from google.cloud import bigquery
 import os
 
+# Initialize BigQuery environment
 client = bigquery.Client()
 dataset_id = 'evect_health'
 dataset_ref = client.dataset(dataset_id)
-
 
 # Retrieve files to be ingested into BigQuery
 # BigQuery tables will reflect JSON file names
 json_file_list=os.listdir('../data')
 table_list=[x.split('.')[0] for x in json_file_list]
 
+# Loop over each of the data files and ingest into a separate BigQuery table
 for json_file, table_name in zip(json_file_list, table_list):
 	filename = '../data/{}'.format(json_file)
-	#table_id = '{}'.format(table_ref)
-	###
 	table_ref = dataset_ref.table(table_name)
 	job_config = bigquery.LoadJobConfig()
-	job_config.autodetect = True
+	job_config.autodetect = True # Use autodetection to define table schema
 	job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
-	###
 	with open(filename, "rb") as source_file:
 	    job = client.load_table_from_file(
 	        source_file,
@@ -28,7 +25,5 @@ for json_file, table_name in zip(json_file_list, table_list):
 	        location="US",  # Must match the destination dataset location.
 	        job_config=job_config,
 	    )  # API request
-###
 	job.result()  # Waits for table load to complete.
-###
 	print("Loaded {} rows into {}:{}.".format(job.output_rows, dataset_id, table_name))
